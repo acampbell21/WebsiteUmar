4e6445ee9f6a521c43bfe9814ed4e36468f1a3c6
Alexander Campbell <acampbell2231@gmail.com>
2019-11-22 11:50:40 -0800

    lets go baby

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var path = require('path');
 
 const app = express();
 var index = require('./routes/index');
@@ -19,8 +18,7 @@ app.use(function(req, res, next) {
 });
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -28,32 +26,11 @@ app.use(express.static('public'));
 
 app.use('/', require('./routes/index'));
 
-// Serve static assets in production
-app.set('view engine', 'jade');
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 module.exports = app;
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use('/static', express.static('client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
