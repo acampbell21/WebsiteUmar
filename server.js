08db0f5064a8d1749a4e2797d918583146b4f1f6
Alexander Campbell <acampbell2231@gmail.com>
2019-11-22 07:15:53 -0800

    favicon island

--- server.js
+++ server.js
@@ -18,15 +18,13 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static('public'));
 
 app.use('/', require('./routes/index'));
 
